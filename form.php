572cf42ecd7f907c34e5994276f8d477d7fdb2be
neyder angarita <neyder6101@gmail.com>
2016-09-26 02:08:10 -0500

    Realiza cargue de varios archivos para analizar

--- form.php
+++ form.php
@@ -4,9 +4,15 @@
 	<title>subir archivo</title>
 </head>
 <body>
-    <form method="post" action="procesar.php" enctype="multipart/form-data" >
+<!--     <form method="post" action="procesar.php" enctype="multipart/form-data" >
         <input type="file" name="foto" />
         <input type="submit" value="Upload" />
+    </form> -->
+
+    <form action="procesar.php" method="post" multipart="" enctype="multipart/form-data">
+        <input type="file" name="img[]" multiple>
+        <input type="submit" value="Upload" >
     </form>
+
 </body>
 </html>
